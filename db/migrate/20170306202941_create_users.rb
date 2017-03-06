56d2c77937e3dbc53ab6fe44877f703eccfe8213
jordan beth <jordanbeth14@gmail.com>
2017-03-06 15:41:49 -0500

    added null and unique qualifications to users

--- db/migrate/20170306202941_create_users.rb
+++ db/migrate/20170306202941_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
+      t.string :username, null: false, unique: true
+      t.string :email, null: false, unique: true
       t.string :password_digest
-      
+
       t.timestamps
     end
   end
